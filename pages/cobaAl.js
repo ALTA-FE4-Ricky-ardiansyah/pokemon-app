7117c9dcf1c5336eb6213332cdd4b6a1546bcf26
ricky ardiansyah <rickyrichk@gmail.com>
2022-04-05 00:44:04 +0700

    fix error2

--- pages/cobaAl.js
+++ pages/cobaAl.js
@@ -1,23 +1,23 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
 
-import CardCoba from "./components/CardCoba";
-export default function cobaAl() {
-  // const [pokemons, setPokemons] = useState([]);
+// import CardCoba from "./components/CardCoba";
+// export default function cobaAl() {
+//   // const [pokemons, setPokemons] = useState([]);
 
-  // useEffect(() => {
-  //   getPokemons("https://pokeapi.co/api/v2/pokemon/?limit=10").then((data) => {
-  //     setPokemons(data);
-  //   });
-  // }, []);
+//   // useEffect(() => {
+//   //   getPokemons("https://pokeapi.co/api/v2/pokemon/?limit=10").then((data) => {
+//   //     setPokemons(data);
+//   //   });
+//   // }, []);
 
-  return (
-    <div>
-      <div>
-        {Object.entries(pokemons)[3] &&
-          Object.entries(pokemons)[3][1].map((pokemon, index) => {
-            return <CardCoba key={index} {...pokemon} />;
-          })}
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <div>
+//         {Object.entries(pokemons)[3] &&
+//           Object.entries(pokemons)[3][1].map((pokemon, index) => {
+//             return <CardCoba key={index} {...pokemon} />;
+//           })}
+//       </div>
+//     </div>
+//   );
+// }
